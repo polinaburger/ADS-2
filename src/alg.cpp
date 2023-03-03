3fabb7c3043566bdd08e51705956d82af8dd1915
Polina Fomicheva <116427492+polinaburger@users.noreply.github.com>
2023-03-03 23:45:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,11 +10,11 @@ double pown(double value, uint16_t n) {
   if (n == 0) {
     return 1;
   }
-  if (n==1) {
+  if (n == 1) {
     return value;
  } else {
-    for(int i=1;i<=n;i++) {
-      value=value*i;
+    for(int i = 1;i <= n;i++) {
+      value = value*i;
     }
     return value;
   }
@@ -34,29 +34,29 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-  if (count<1) {
+  if (count < 1) {
     return 1;
   } else {
-    int a=0;
-    for (i=0; i<count; i++) {
-      a=a+caclItem(x,i);
+    int a = 0;
+    for (int i = 0; i < count; i++) {
+      a = a + calcItem(x,i);
     }
     return a;
   }
 }
 
 double sinn(double x, uint16_t count) {
-  int b=0;
-  for (i=1; i<=count; i++) {
-    b=b+(pown(-1,i)*pown(x,2i-1)/(fact(2*i-1));
+  uint16_t b = 0;
+  for (int i = 1; i <= count; i++) {
+    b=b + (pown(-1,i) * pown(x,2i -1 ) / (fact(2 * i - 1)));
          }
   return b;
 }
 
 double cosn(double x, uint16_t count) {
-  int c=0;
-  for (int i=1; i<=count; i++) {
-    c=c+(pown(-1;i)*pown(x, 2i-2)/fact(2*i-2))
+  int c = 0;
+  for (int i = 1; i <= count; i++) {
+    c = c + (pown(-1;i) * pown(x, 2i - 2) / fact(2 * i - 2));
     }
   return c;
 }
